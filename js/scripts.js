5ef22c60b908b15e10cd6533f7314a870288820a
Laura Shapiro <lauraemilys323@gmail.com>
2021-01-31 01:22:09 -0800

    add a function for submit button. Currently doesn't work

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
 const littleBit = $("input:radio[name=littleBit]:checked").val();
+
+let submit = "Wow you should learn all the languages!"
+
+$ function (String(submit)){
+    return "Wow you should learn all the languages!"
+}
+    
