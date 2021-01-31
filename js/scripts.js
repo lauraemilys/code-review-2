d1232116f43e1180428f2e8f7602e28a6bb62219
Laura Shapiro <lauraemilys323@gmail.com>
2021-01-31 01:38:38 -0800

    clears up redundancies in my css styling, removes faulty js functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 const littleBit = $("input:radio[name=littleBit]:checked").val();
 
-let submit = "Wow you should learn all the languages!"
-
 $ function (String(submit)){
-    return "Wow you should learn all the languages!"
+    return "Wow you should learn all the languages!";
 }
     
